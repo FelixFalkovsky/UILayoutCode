2b536943c106afc25bf1be6ac6b78803a089ca97
FelixFalkovsky <falkovskyfelix@gmail.com>
2021-11-22 14:10:46 +0300

    init commit

---
+++ UILayoutCode.git/UILayoutCode.gitTests/UILayoutCode_gitTests.swift
@@ -0,0 +1,33 @@
+//
+//  UILayoutCode_gitTests.swift
+//  UILayoutCode.gitTests
+//
+//  Created by Felix on 22.11.2021.
+//
+
+import XCTest
+@testable import git_github_com_FelixFalkovsky_UILayoutCode_git
+
+class UILayoutCode_gitTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
